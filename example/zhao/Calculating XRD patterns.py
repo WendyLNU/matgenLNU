03a72e46d0b2c183f85c1ecaa8a4b8f9214a4001
WendyLNU <40648626+wendylnu@users.noreply.github.com>
2018-07-05 16:15:19 +0800

    Some modification.

--- example/zhao/Calculating XRD patterns.py
+++ example/zhao/Calculating XRD patterns.py
@@ -1,14 +1,25 @@
 from pymatgen import Lattice, Structure
 from pymatgen.analysis.diffraction.xrd import XRDCalculator
 from IPython.display import Image, display
+from matplotlib import pyplot
 
 
 # Create CsCl structure
 a = 4.209 #Angstrom(埃:一个长度单位，用来表示原子尺寸、键长和电磁波波长。)
 latt = Lattice.cubic(a)
+
+#latt:点阵一个长度为a的立方体
+#species：物质，这里是["Cs", "Cl"]
+#坐标：三位数组的列表，表示物质的坐标
 structure = Structure(latt, ["Cs", "Cl"], [[0, 0, 0], [0.5, 0.5, 0.5]])
+
 c = XRDCalculator()
-c.get_plot(structure, two_theta_range=(0, 90), annotate_peaks=True, ax=None, with_labels=True, fontsize=16)
+
+#get_plot返回的结果就是一个图片，可以用pyplot展示和保存
+image = c.get_plot(structure, two_theta_range=(0, 90), annotate_peaks=True, ax=None, with_labels=True, fontsize=16)
+
+pyplot.show(image)
+pyplot.savefig("alpha CsCl.jpg")
 #display(Image(filename=('./PDF - alpha CsCl.png')))
 a = 6.923 #Angstrom
 latt = Lattice.cubic(a)
@@ -18,3 +29,5 @@ structure = Structure(latt, ["Cs", "Cs", "Cs", "Cs", "Cl", "Cl", "Cl", "Cl"],
 
 c.get_plot(structure, two_theta_range=(0, 90), annotate_peaks=True, ax=None, with_labels=True, fontsize=16)
 #display(Image(filename=('./PDF - beta CsCl.png')))这段代码display我还没有弄明白，帮我看看哪里出问题了
+pyplot.show(image)
+pyplot.savefig("beta CsCl.jpg")
